dff8b811efc80fe9be9c64156398bb91932e676c
Light-Center <pconstance106@mycr.redwoods.edu>
2022-09-02 12:58:18 -0700

    Do you like π? Java-Assignment-002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,26 @@
-import java.util.Scanner;
+/**
+ *
+ * @author Trevor Hartman
+ * @author Paul Constance
+ *
+ * @since Version 1.0
+ *
+ */
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+
+        int polygonSides = 420;
+
+        double polygonB = 360.0 / polygonSides;
+        double polygonA = polygonB / 2;
+
+        double polygonLength = 2 * (Math.sin(Math.toRadians(polygonA)));
+
+        double polygonPerimeter = polygonLength * polygonSides;
+
+        System.out.print("I like π, ");
+        System.out.print(polygonPerimeter / 2);
 
     }
 }
